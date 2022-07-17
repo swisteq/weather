ade77ea1fdf3a074432bd3e8f8a72a99c2bff75e
swisteq <damianskarbon@gmail.com>
2022-07-17 13:51:18 +0200

    WA-001 create UserInterface class

--- src/main/java/com/sda/Application.java
+++ src/main/java/com/sda/Application.java
@@ -2,7 +2,7 @@ package com.sda;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("Michal tu byl");
+        UserInterface userInterface = new UserInterface();
+        userInterface.run();
     }
 }
